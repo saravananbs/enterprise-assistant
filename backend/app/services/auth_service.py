d8defe447d6a49c5eb73baeae7476eae0273c0c3
saravanan <bssaravanan247@gmail.com>
2026-01-21 10:54:02 +0530

    fix: handle unauthorized email

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound
 from ..my_agents.utils.db.models import Employee
 from ..utils.generate_otp import generate_otp
 from ..utils.otp_store import store_otp, verify_otp
@@ -8,7 +9,10 @@ from ..utils.send_email_otp import send_otp_email
 async def login_with_email(db: AsyncSession, email: str):
     stmt = select(Employee).where(Employee.email == email)
     res = await db.execute(stmt)
-    employee = res.scalar_one()
+    try:
+        employee = res.scalar_one()
+    except NoResultFound:
+        return None
     if not employee:
         return None
     otp = await generate_otp()
